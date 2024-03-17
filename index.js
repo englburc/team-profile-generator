cca75f0e54126f2665125da7a479b0de4a550bcf
englburc <teacher.levan@gmail.com>
2024-03-17 19:10:20 +0000

    index.js fucntions init and options added

--- index.js
+++ index.js
@@ -12,4 +12,61 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+let EmployeeArray = [];
 
+
+function init() {
+    const manager_questions = [
+        {
+            type: "input",
+            message: "Manager's name?",
+            name: "managerName",
+            required: true
+        },
+        {
+            type: "input",
+            message: "Manager's Id?",
+            name: "managerId",
+            required: true
+        },
+        {
+            type: "input",
+            message: "Manager's email?",
+            name: "managerEmail",
+            required: true,
+            validate: function (email) {
+                // Regex mail check (return true if valid mail)
+                return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+            }
+        },
+        {
+            type: "input",
+            message: "Office number?",
+            name: "officeNumber",
+            required: true
+        }
+    ]
+    inquirer
+        .prompt(manager_questions)
+        .then((response) => {
+            EmployeeArray.push(new Manager(name = response.managerName, id = response.managerId, email = response.managerEmail, officeNumber = response.officeNumber));
+        }
+        )
+        .then(() => option())
+}
+
+function option() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            if (response.option === 'Add an engineer') {
+                addEngineer();
+            }
+            else if (response.option === 'Add an intern') {
+                addIntern();
+            }
+            else {
+                writeToFile(outputPath, render(EmployeeArray));
+            }
+        })
+}
